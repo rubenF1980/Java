9293954e718878d6e0797bf5cac8d5189d3d93e3
rubenF1980 <43235289+rubenF1980@users.noreply.github.com>
2018-10-03 11:00:28 +0200

    Add files via upload

---
+++ For_mejorado.java
@@ -0,0 +1,22 @@
+/*
+ * 
+ */
+
+public class For_mejorado {
+    
+    public static void main(String[] args) {
+      
+        int matriz[]={5,6,8,3,4,2,1};
+        int suma=0;
+        for (int numero :matriz) {
+            
+           suma+=numero;
+            
+        }
+        System.out.println("Elemento :" + suma);
+    
+    
+    
+    
+    }//main    
+}
